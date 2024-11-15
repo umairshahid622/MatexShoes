2b437a227502fa1cebae4b03ef32fcfd6aa0810a
umairshahid622 <93341787+umairshahid622@users.noreply.github.com>
2024-11-16 01:23:24 +0500

    j

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(
       "http://localhost:3003", // Alternative local port
       "https://matexshoes.vercel.app", // Your future frontend URL
       "https://matexshoesapi.vercel.app", // Your future backend URL
-      "https://matex-shoes.vercel.app/",
+      "https://matex-shoes.vercel.app",
     ],
     credentials: true,
   })
